bc3346b7058dc1cee3a1ac74114011f612964ca1
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-12-23 13:45:23 +0530

    cleanups to readme and project-setup.sh

--- project-setup.sh
+++ project-setup.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-echo 'starting the project'
+echo 'Creating env file'
 cp .env.example .env
-echo 'Installing Composer'
+
+echo 'Installing composer dependencies'
 composer install
-echo 'Installing npm'
+
+echo 'Installing npm dependencies'
 npm install
 npm run dev
+
 echo 'Generating application key'
 php artisan key:generate
-echo 'Setting Pre-commit hooks'
+
+echo 'Setting up pre-commit hooks'
 cp git-hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
