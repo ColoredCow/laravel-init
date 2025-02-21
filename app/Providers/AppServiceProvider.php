416ed8815f2431d846a75e17677fcd5ac9df46ec
Abhinegi2 <negiabhishek253@gmail.com>
2025-02-21 14:13:46 +0530

    Refactor CI workflow + dependencies and improve test reporting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,14 +23,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        Gate::after(function ($user, $ability) {
-           if($user->hasRole('admin')){
-               return true;
-           }
-           return false;
+        Gate::after(function ($user) {
+            if ($user->hasRole('admin')) {
+                return true;
+            }
+            return false;
         });
     }
 }
