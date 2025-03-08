c06f63408c073fa31c3900f14206a0606f84091b
Nishant <nishantumu@gmail.com>
2025-03-08 18:00:30 +0530

    updated password reset test cases

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -3,18 +3,26 @@
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
 
 test('reset password link can be requested', function () {
+    $this->withoutMiddleware();
     Notification::fake();
 
     $user = User::factory()->create();
 
-    $this->post('/forgot-password', ['email' => $user->email]);
+    // $response = $this->postJson(route('/forgot-password'), ['email' => $user->email]);
+    $response = $this->postJson('/forgot-password', ['email' => $user->email]);
+
+    $response->assertStatus(200)
+        ->assertJson(['status' => trans(Password::RESET_LINK_SENT)]);
 
     Notification::assertSentTo($user, ResetPassword::class);
 });
 
+
 test('password can be reset with valid token', function () {
+    $this->withoutMiddleware();
     Notification::fake();
 
     $user = User::factory()->create();
