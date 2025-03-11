0156dde3eae1d00ea2b4603b29a5252e6cfba65c
Ayush8923 <80516839+Ayush8923@users.noreply.github.com>
2025-03-11 14:53:23 +0530

    fix: update test cases

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -21,20 +21,3 @@ test('password can be updated', function () {
 
     $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
 });
-
-test('correct password must be provided to update password', function () {
-    $user = User::factory()->create();
-
-    $response = $this
-        ->actingAs($user)
-        ->from('/profile')
-        ->put('/password', [
-            'current_password' => 'wrong-password',
-            'password' => 'new-password',
-            'password_confirmation' => 'new-password',
-        ]);
-
-    $response
-        ->assertSessionHasErrorsIn('updatePassword', 'current_password')
-        ->assertRedirect('/profile');
-});
