c0ada58937c7963634f32a3f23514026e0dc9386
Nishant <nishantumu@gmail.com>
2025-03-18 12:54:45 +0530

    added roles and permissions

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run()
+    {
+        // Create Roles if they don't exist
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+
+        // Create Permissions if they don't exist
+        $editArticles = Permission::firstOrCreate(['name' => 'edit articles']);
+        $deleteArticles = Permission::firstOrCreate(['name' => 'delete articles']);
+
+        // Assign permissions to roles
+        $adminRole->givePermissionTo([$editArticles, $deleteArticles]);
+        $userRole->givePermissionTo($editArticles);
+
+        $this->command->info('Roles and permissions seeded successfully!');
+    }
+}
