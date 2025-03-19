df5a2d876b601f377561b94b61de02a44078d1eb
Nishant <nishantumu@gmail.com>
2025-03-19 19:01:17 +0530

    fix(*) remove roles

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::with('roles')->get();
+        return response()->json($users);
+    }
+
+    public function updateRoles(Request $request, User $user)
+    {
+        $request->validate([
+            'roles' => 'array',
+            'roles.*' => 'exists:roles,name',
+        ]);
+
+        $user->syncRoles($request->roles);
+        return response()->json(['message' => 'Roles updated successfully']);
+    }
+
+    public function getRoles()
+    {
+        $roles = Role::all();
+        return response()->json($roles);
+    }
+}
