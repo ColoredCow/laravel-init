44aa452741219bd9e39a5b1ce7166bc9ec3bfb79
belwalshubham <belwalshubham08@gmail.com>
2025-03-24 15:12:27 +0530

    code cleanups

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,13 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    // User and Role Management
     Route::get('/users', [UserController::class, 'index']);
     Route::put('/users/{user}/roles', [UserController::class, 'updateRoles']);
     Route::get('/roles', [UserController::class, 'getRoles']);
-});
 
-Route::middleware(['auth:sanctum'])->group(function () {
+    // Profile Management
     Route::put('/profile', [ProfileController::class, 'update']);
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
 });
+
