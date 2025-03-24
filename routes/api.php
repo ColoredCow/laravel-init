4e15534dc21656ff3d6fe2c9f4ad8b7c600e3099
Ayush <80516839+Ayush8923@users.noreply.github.com>
2025-03-24 15:06:09 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::patch('/profile', 'update')->name('profile.update');
         Route::delete('/profile', 'destroy')->name('profile.destroy');
     });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index')->name('users.show');
+        Route::patch('/users/{user}/roles', 'updateRoles')->name('users.update');
+        Route::delete('/roles', 'getRoles')->name('users.roles');
+    });
 });
