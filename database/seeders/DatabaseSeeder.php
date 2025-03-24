f15558dca9b370dedab2883a357120f2b6cf9498
Nishant <nishantumu@gmail.com>
2025-03-24 16:37:16 +0530

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         $this->call(UserSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
     }
 }
